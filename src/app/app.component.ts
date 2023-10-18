bd28ce296957f1705e7c098a57e140d57d7b09e2
ed <eapesteguiinocente@gmail.com>
2023-10-18 18:10:26 -0500

    funciona casi todo bien

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'examen';
+  selectedCategory = 'comida';
+  monto: number = 0;
+  gastos: { [key: string]: number } = {
+    comida: 0,
+    estudios: 0,
+    transporte: 0,
+    ocio: 0
+  };
+  totalGastos = 0;
+  consejos = [
+    'Ahorra al menos el 20% de tus ingresos.',
+    'Evita gastos innecesarios.',
+    'Crea un presupuesto mensual y síguelo.'
+  ];
+
+  agregarGasto() {
+    
+    this.gastos[this.selectedCategory] += this.monto;
+    this.totalGastos = this.gastos['comida'] + this.gastos['estudios'] + this.gastos['transporte'] + this.gastos['ocio'];
+    alert(this.totalGastos)
+  }
+
+  ngOnInit() {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ExamenComponent } from './examen/examen.component';
 
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ExamenComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
