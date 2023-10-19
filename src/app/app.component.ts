3600b6130fbba15a25b361a505da678d45f3506d
ed <eapesteguiinocente@gmail.com>
2023-10-18 19:37:38 -0500

    listo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,19 +16,24 @@ export class AppComponent {
     ocio: 0
   };
   totalGastos = 0;
-  consejos = [
-    'Ahorra al menos el 20% de tus ingresos.',
-    'Evita gastos innecesarios.',
-    'Crea un presupuesto mensual y síguelo.'
+  consejosClave = [
+    'Evita las compras por impulso. ¡No seas impulsivo!',
+    'Detalla todos tus gastos mensuales. ¡Que no se te escape nada!',
+    'Evita los llamados gastos hormiga. ¡No necesitas Spotify para vivir!'
+  ];
+  mensajesMotivacionales = [
+    '¡Cada dia el sueño del departamento esta más cerca!',
+    'Ya lo dijo Julio Velarde: ¡Ahorro es progreso!',
+    '¡El que la sigue, la consige!'
   ];
 
   agregarGasto() {
-    
-    this.gastos[this.selectedCategory] += this.monto;
-    this.totalGastos = this.gastos['comida'] + this.gastos['estudios'] + this.gastos['transporte'] + this.gastos['ocio'];
-    alert(this.totalGastos)
-  }
-
-  ngOnInit() {
+    if (this.monto <= 0) {
+      alert('¡Profe! ¡Eso no se hace! No se debe ingresar S/0 o negativo');
+    } else {
+      this.gastos[this.selectedCategory] += this.monto;
+      this.totalGastos = this.gastos['comida'] + this.gastos['estudios'] + this.gastos['transporte'] + this.gastos['ocio'];
+      alert('Se ha registrado el nuevo gasto en la categoría: ' + this.selectedCategory );
+    }   
   }
 }
